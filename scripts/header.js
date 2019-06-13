a089dedefe9188bc57039313e2f91b4984be61ae
Drew Guarnera <dguarner@kent.edu>
2019-06-13 16:25:14 -0400

    Make header compatible with IE 11

--- scripts/header.js
+++ scripts/header.js
@@ -1,93 +1,93 @@
 function writeHeader() {
-    return `<!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-    <div class="container">
-        <a class="navbar-brand" href="index.html">ICSME 2019</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Program</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="keynotes.html">Keynotes</a>
-                    <a class="dropdown-item" href="accepted_papers.html">Accepted Papers</a>
-                </div>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Call for Papers</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="cfp_research_track.html">Research Track</a>
-                    <a class="dropdown-item" href="cfp_short_papers_track.html">Short Paper Track</a>
-                    <a class="dropdown-item" href="cfp_late_breaking_ideas_track.html">Late Breaking Ideas Track</a>
-                    <a class="dropdown-item" href="cfp_tool_demo_track.html">Tool Demo Track</a>
-                    <a class="dropdown-item" href="cfp_industry_track.html">Industry Track</a>
-                    <a class="dropdown-item" href="cfp_artifacts_track.html">Artifacts Track</a>
-                    <a class="dropdown-item" href="cfp_journal_first_track.html">Journal First Track</a>
-                    <a class="dropdown-item" href="cfp_doctoral_symposium.html">Doctoral Symposium</a>
-                </div>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Team</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="organizing_committee.html">Organizing Committee</a>
-                    <a class="dropdown-item" href="program_committee.html">Program Committee</a>
-                </div>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Co-Located Events</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="http://www.ieee-scam.org/2019/" target="_BLANK">SCAM</a>
-                    <a class="dropdown-item" href="http://vissoft19.dcc.uchile.cl/" target="_BLANK">VISSOFT</a>
-                </div>
-            </li>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Venue & Travel</a>    
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="venue.html">Venue</a>
-                </div>
-            </li>
-
-            <!-- HIDE REGISTRATION
-            <li class="nav-item">
-                <a class="nav-link" href="#">Registration</a>
-            </li>
-            -->
-
-            <!-- HIDE KEYNOTE
-            <li class="nav-item">
-                <a class="nav-link" href="#">Keynote</a>
-            </li>
-            -->
-
-            <!-- HIDE PROGRAM
-            <li class="nav-item">
-                <a class="nav-link" href="#">Program</a>
-            </li>
-            -->
-            
-            <!-- HIDE SOCIAL EVENTS
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Social Events</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                </div>
-            </li>
-            -->
-
-        </ul>
-        </div>
-    </div>
-    </nav>
-
-    <!-- Header Content -->
-    <div><img src="assets/banners/icsme2019_banner.jpg" class="img-fluid" alt="Responsive image"></div>
-    `;
+    return '<!-- Navigation -->\
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">\
+    <div class="container">\
+        <a class="navbar-brand" href="index.html">ICSME 2019</a>\
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
+            <span class="navbar-toggler-icon"></span>\
+        </button>\
+\
+        <div class="collapse navbar-collapse" id="navbarResponsive">\
+        <ul class="navbar-nav ml-auto">\
+             <li class="nav-item dropdown">\
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Program</a>\
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
+                    <a class="dropdown-item" href="keynotes.html">Keynotes</a>\
+                    <a class="dropdown-item" href="accepted_papers.html">Accepted Papers</a>\
+                </div>\
+            </li>\
+\
+            <li class="nav-item dropdown">\
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Call for Papers</a>\
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
+                    <a class="dropdown-item" href="cfp_research_track.html">Research Track</a>\
+                    <a class="dropdown-item" href="cfp_short_papers_track.html">Short Paper Track</a>\
+                    <a class="dropdown-item" href="cfp_late_breaking_ideas_track.html">Late Breaking Ideas Track</a>\
+                    <a class="dropdown-item" href="cfp_tool_demo_track.html">Tool Demo Track</a>\
+                    <a class="dropdown-item" href="cfp_industry_track.html">Industry Track</a>\
+                    <a class="dropdown-item" href="cfp_artifacts_track.html">Artifacts Track</a>\
+                    <a class="dropdown-item" href="cfp_journal_first_track.html">Journal First Track</a>\
+                    <a class="dropdown-item" href="cfp_doctoral_symposium.html">Doctoral Symposium</a>\
+                </div>\
+            </li>\
+\
+            <li class="nav-item dropdown">\
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Team</a>\
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
+                    <a class="dropdown-item" href="organizing_committee.html">Organizing Committee</a>\
+                    <a class="dropdown-item" href="program_committee.html">Program Committee</a>\
+                </div>\
+            </li>\
+\
+            <li class="nav-item dropdown">\
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Co-Located Events</a>\
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
+                    <a class="dropdown-item" href="http://www.ieee-scam.org/2019/" target="_BLANK">SCAM</a>\
+                    <a class="dropdown-item" href="http://vissoft19.dcc.uchile.cl/" target="_BLANK">VISSOFT</a>\
+                </div>\
+            </li>\
+\
+            <li class="nav-item dropdown">\
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Venue & Travel</a>\
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
+                    <a class="dropdown-item" href="venue.html">Venue</a>\
+                </div>\
+            </li>\
+\
+            <!-- HIDE REGISTRATION\
+            <li class="nav-item">\
+                <a class="nav-link" href="#">Registration</a>\
+            </li>\
+            -->\
+\
+            <!-- HIDE KEYNOTE\
+            <li class="nav-item">\
+                <a class="nav-link" href="#">Keynote</a>\
+            </li>\
+            -->\
+\
+            <!-- HIDE PROGRAM\
+            <li class="nav-item">\
+                <a class="nav-link" href="#">Program</a>\
+            </li>\
+            -->\
+\
+            <!-- HIDE SOCIAL EVENTS\
+            <li class="nav-item dropdown">\
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Social Events</a>\
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
+                    <a class="dropdown-item" href="#">Action</a>\
+                    <a class="dropdown-item" href="#">Another action</a>\
+                </div>\
+            </li>\
+            -->\
+\
+        </ul>\
+        </div>\
+    </div>\
+    </nav>\
+\
+    <!-- Header Content -->\
+    <div><img src="assets/banners/icsme2019_banner.jpg" class="img-fluid" alt="Responsive image"></div>\
+    ';
 }
