6431932bb22a387ab132051ebb41471dd6024dd1
Drew Guarnera <dguarner@kent.edu>
2019-06-26 16:22:55 -0400

    Add code of conduct and fix lgbtiq formatting

--- scripts/header.js
+++ scripts/header.js
@@ -56,7 +56,7 @@ function writeHeader() {
             <li class="nav-item dropdown">\
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Social</a>\
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">\
-                    <a class="dropdown-item" href="lbgtiq_event.html">LGBTIQ Lunch Event</a>\
+                    <a class="dropdown-item" href="lgbtiq_event.html">LGBTIQ Lunch Event</a>\
                 </div>\
             </li>\
 \
